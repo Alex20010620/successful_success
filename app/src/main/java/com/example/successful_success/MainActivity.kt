e7a1a4f3e4ac1ce9608525dd84f9ee7740818209
Alex20010620 <58935675+Alex20010620@users.noreply.github.com>
2022-07-01 01:57:04 +0300

    Powered by Fragments With single activity/

--- app/src/main/java/com/example/successful_success/MainActivity.kt
+++ app/src/main/java/com/example/successful_success/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.successful_success
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
-import com.example.successful_success.City_Map_Activity as City_Map_Activity1
 
+class MainActivity() : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
 
+ /*   override fun writeToParcel(parcel: Parcel, flags: Int) {
 
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
         val button1: ImageView = findViewById(R.id.city)
         button1.setOnClickListener{
             val intent = Intent(this, com.example.successful_success.City_Map_Activity::class.java)
@@ -32,10 +50,10 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-/*private fun ImageView.setOnClickListener(button1: ImageView) {
+private fun ImageView.setOnClickListener(button1: ImageView) {
     @Override
     fun  onClick(view: View) {
-    button1 = Intent(this, City_Map_Activity::class.java)
+    button1 = Intent(this, City_Map_Activity1::class.java)
     startActivity(com.example.successful_success.City_Map_Activity)}
 
 } */
